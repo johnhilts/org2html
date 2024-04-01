3c1f523b49c66bdac1d416b40d023afe930f41c4
John Hilts <johnhilts@live.com>
2024-03-31 21:00:01 -0700

    org2html - fixed elisp http post code

--- http.el
+++ http.el
@@ -1,9 +1,12 @@
-(defun http-post (url data)
+(defun my/http-post (url data)
   "Send DATA to URL as a POST request."
   (let ((url-request-method "POST")
-        (url-request-extra-headers '(("Content-Type" . "text/plain")))
-        (url-request-data (format "org-text=%S" data)))
+     	(url-request-extra-headers `(("Content-Type" . "application/x-www-form-urlencoded")))
+        (url-request-data (format "org-text=%s" data)))
     (url-retrieve url (lambda (status) (switch-to-buffer (current-buffer))))))
 
-(defun example-http-post ()
-    (http-post "http://192.168.1.18:5096/show" "* One\r\n** Two"))
+(defun my/example-http-post ()
+    (my/http-post "http://192.168.1.18:5096/receive" "* One\r\n** Two"))
+
+(defun my/show-html ()
+  (url-retrieve "http://192.168.1.18:5096/show.html" (lambda (status) (switch-to-buffer (current-buffer)))))
