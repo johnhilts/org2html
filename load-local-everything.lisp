2b2e5a8511599e2117921eb2cadeda10dea30968
John Hilts <johnhilts@live.com>
2024-04-06 09:48:54 -0700

    org2html - another fix I missed

--- load-local-everything.lisp
+++ load-local-everything.lisp
@@ -13,13 +13,13 @@
   (load-local-web-core)
   (print "web-core loaded")
 
-  (load "/home/jfh/code/lisp/source/org2html/web-app/load-local-web-app.lisp")
-  (load-local-web-app)
-  (print "web-app loaded")
-
   (load "/home/jfh/code/lisp/source/org2html/org2html/load-local-org2html.lisp")
   (load-local-org2html)
   (print "org2html loaded")
+
+  (load "/home/jfh/code/lisp/source/org2html/web-app/load-local-web-app.lisp")
+  (load-local-web-app)
+  (print "web-app loaded")
 
   (swank:set-default-directory "/home/jfh/code/lisp/source/org2html/")
   (push #p"/home/jfh/code/lisp/source/org2html/" asdf:*central-registry*)
