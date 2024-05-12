dc3c463b6fa8a99585f0ee98d151c1731d137684
John Hilts <johnhilts@live.com>
2024-05-12 09:24:15 -0700

    org2html - refactor for easier testing

--- web-app/common/page-handlers.lisp
+++ web-app/common/page-handlers.lisp
@@ -44,30 +44,39 @@
 	       (:div (:button "Add")))))))))
 
 (defun use-jira (org-text)
+  "Orchestrates: getting Jira markdown from org mode text, then outputting the Jira markdown embedded in HTML. It's meant to be copy/pasted into a Jira description."
   (let* ((jira-markdown (org2html:build-tree-for-jira (if (zerop (length org-text)) org2html::*test-text* org-text))))
     (who:with-html-output-to-string
         (*standard-output* nil :prologue t :indent t)
-      (:html (:body (:div (:text-area (who:str jira-markdown)))))))))
+      (:html (:body (:div (:text-area (who:str jira-markdown))))))))
+
+(defun use-html (org-text use-light-mode)
+  "Orchestrates: getting cl-who tree from org mode text, then saving it as an HTML file that can be served from the web app. It's meant to be viewed in a browser."
+  (let* ((all-html (org2html:build-tree (org2html:parse-org-text (if (zerop (length org-text)) org2html::*test-text* org-text) #'format-for-web)))
+         (html-head (cdr (assoc 'org2html:html-head all-html)))
+         (html-body (cdr (assoc 'org2html:html-body all-html))))
+    (org2html:save-as-html (eval
+                            `(who:with-html-output-to-string
+                                 (*standard-output* nil :prologue t :indent t)
+                               (:html
+                                (who:str ,(common-header html-head use-light-mode))
+                                (:body
+                                 (:div
+                                  ,@html-body))))))
+    (who:with-html-output-to-string
+        (*standard-output* nil :prologue t :indent t)
+      (:html (:body (:div (:a :href #1="http://192.168.1.18:5096/show.html" #1#)))))))
+
+(defun save-received-text-and-render (org-text use-light-mode use-jira)
+  "orchestrates either 1. org -> Jira markdown conversion, or 2. org -> html conversion then saves as a file and returns a url to that file."
+  (if (string= "t" use-jira)
+      (use-jira org-text)
+      (use-html org-text use-light-mode)))
 
 (tbnl:define-easy-handler (receive :uri "/receive") (use-light-mode use-jira)
   "http post handler that orchestrates org -> html conversion then saves as a file and returns a url to that file."
   (let ((org-text (tbnl:post-parameter "org-text")))
-    (if (string= "t" use-jira)
-        (use-jira org-text)
-        (let* ((all-html (org2html:build-tree (org2html:parse-org-text (if (zerop (length org-text)) org2html::*test-text* org-text) #'format-for-web)))
-               (html-head (cdr (assoc 'org2html:html-head all-html)))
-               (html-body (cdr (assoc 'org2html:html-body all-html))))
-          (org2html:save-as-html (eval
-                                  `(who:with-html-output-to-string
-                                       (*standard-output* nil :prologue t :indent t)
-                                     (:html
-                                      (who:str ,(common-header html-head use-light-mode))
-                                      (:body
-                                       (:div
-                                        ,@html-body))))))
-          (who:with-html-output-to-string
-              (*standard-output* nil :prologue t :indent t)
-            (:html (:body (:div (:a :href #1="http://192.168.1.18:5096/show.html" #1#)))))))))
+    (save-received-text-and-render org-text use-light-mode use-jira)))
 
 (tbnl:define-easy-handler (version-page :uri "/version") ()
   (who:with-html-output-to-string
